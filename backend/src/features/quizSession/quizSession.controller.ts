f7bc7776af35766820d8ca9156bf94c7fd8f7182
Jakov Knezevic <knezevic.jakov@gmail.com>
2026-08-18 12:24:35 +0200

    Reworked quiz session endpoints

--- backend/src/features/quizSession/quizSession.controller.ts
+++ backend/src/features/quizSession/quizSession.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Get,
+	Param,
+	Patch,
+	Post,
+	Query,
+} from '@nestjs/common';
 import { ParseCuid2Pipe } from 'src/common/pipes/parseCuid2.pipe';
 import { QuizSessionService } from './quizSession.service';
 import { UpdateQuizSessionStatusDto } from 'src/common/dtos/quizSession/patchQuizSessionStatus.dto';
@@ -8,28 +16,30 @@ import { GetQuizSessionDto } from 'src/common/dtos/quizSession/getQuizSession.dt
 export class QuizSessionController {
 	constructor(private readonly quizSessionService: QuizSessionService) {}
 
-	@Get('all/:userId')
+	@Get()
+	// TODO: @CurrentUser() user: User -> add this decorator later on after auth is implemented to extract user from request
+	// Reason is security
 	getQuizSessions(
-		@Param('userId', ParseCuid2Pipe) userId: string,
+		@Query('userId', ParseCuid2Pipe) userId: string,
 	): Promise<GetQuizSessionDto[]> {
 		return this.quizSessionService.getQuizSessions(userId);
 	}
 
-	@Get('session/:quizSessionId')
+	@Get(':quizSessionId')
 	getQuizSession(
 		@Param('quizSessionId', ParseCuid2Pipe) quizSessionId: string,
 	): Promise<GetQuizSessionDto> {
 		return this.quizSessionService.getQuizSession(quizSessionId);
 	}
 
-	@Post('session/:userId')
+	@Post(':userId')
 	createQuizSession(
 		@Param('userId', ParseCuid2Pipe) userId: string,
 	): Promise<GetQuizSessionDto> {
 		return this.quizSessionService.createQuizSession(userId);
 	}
 
-	@Patch('session/:quizSessionId')
+	@Patch(':quizSessionId')
 	updateQuizSessionStatus(
 		@Param('quizSessionId', ParseCuid2Pipe) quizSessionId: string,
 		@Body() body: UpdateQuizSessionStatusDto,

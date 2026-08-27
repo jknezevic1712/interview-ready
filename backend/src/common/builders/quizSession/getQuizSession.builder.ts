0ef697f66ab1a475a13980ecf5b1f43273f742ff
Jakov Knezevic <knezevic.jakov@gmail.com>
2026-08-27 20:14:27 +0200

    Quiz session DTO builders added

---
+++ backend/src/common/builders/quizSession/getQuizSession.builder.ts
@@ -0,0 +1,24 @@
+import { GetQuizSessionBaseBuilder } from './getQuizSessionBase.builder';
+
+import type { GetQuizSessionDto } from 'src/common/dtos/quizSession/getQuizSession.dto';
+
+export class GetQuizSessionBuilder
+	extends GetQuizSessionBaseBuilder
+	implements GetQuizSessionDto
+{
+	responses: GetQuizSessionDto['responses'] = [];
+
+	withResponses(responses: GetQuizSessionDto['responses']) {
+		this.responses = responses;
+		return this;
+	}
+
+	build(): GetQuizSessionDto {
+		return {
+			...this.buildBase(),
+			responses: this.responses,
+		};
+	}
+}
+
+export const buildGetQuizSession = () => new GetQuizSessionBuilder();

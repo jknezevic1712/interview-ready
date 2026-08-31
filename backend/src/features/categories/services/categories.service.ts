f430634e3f62979ad3656e9f791e55b35e54d752
Jakov Knezevic <knezevic.jakov@gmail.com>
2026-08-31 21:07:09 +0200

    Added caching to categories

--- backend/src/features/categories/services/categories.service.ts
+++ backend/src/features/categories/services/categories.service.ts
@@ -1,8 +1,13 @@
 import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { CategoryResponse } from 'src/common/dtos/categories/categoryResponse.dto';
+import { CacheService } from 'src/infrastructure/cache/cache.service';
+import {
+	CACHE_KEYS,
+	CACHE_TTL,
+} from 'src/infrastructure/cache/utilities/constants';
 import { toCategoryResponse } from '../mappers/categories.mapper';
 import { CATEGORIES_REPOSITORY } from '../tokens/categories.token';
 
-import type { CategoryResponse } from 'src/common/dtos/categories/categoryResponse.dto';
 import type { ICategoriesRepository } from '../contracts/categories.repository.contract';
 
 @Injectable()
@@ -10,11 +15,25 @@ export class CategoriesService {
 	constructor(
 		@Inject(CATEGORIES_REPOSITORY)
 		private readonly categoriesRepository: ICategoriesRepository,
+		private readonly cacheService: CacheService,
 	) {}
 
 	async getAll(): Promise<CategoryResponse[]> {
+		const cached = await this.cacheService.get<CategoryResponse[]>(
+			CACHE_KEYS.categories.getAll,
+		);
+		if (cached) return cached;
+
 		const categories = await this.categoriesRepository.getAll();
-		return categories.map(toCategoryResponse);
+		const mappedCategories = categories.map(toCategoryResponse);
+
+		await this.cacheService.set(
+			CACHE_KEYS.categories.getAll,
+			mappedCategories,
+			CACHE_TTL.categories.getAll,
+		);
+
+		return mappedCategories;
 	}
 
 	async getById(id: string): Promise<CategoryResponse | null> {

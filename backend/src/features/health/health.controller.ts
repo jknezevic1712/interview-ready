66bd0b1a6ffe7bbc117670e4506db1cfdf6c65bc
Jakov Knezevic <knezevic.jakov@gmail.com>
2026-09-02 11:25:14 +0200

    Added small DTO for health endpoint

--- backend/src/features/health/health.controller.ts
+++ backend/src/features/health/health.controller.ts
@@ -2,12 +2,14 @@ import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Public } from 'src/common/decorators/public.decorator';
 
+import type { HealthResponse } from 'src/common/dtos/health/healthResponse.dto';
+
 @ApiTags('Health')
 @Controller('health')
 export class HealthController {
 	@Public()
 	@Get()
-	check() {
+	check(): HealthResponse {
 		return { status: 'ok' };
 	}
 }

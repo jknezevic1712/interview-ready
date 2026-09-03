a7d1304c48ebb570882713308b2ec2f0aa4ba79f
Jakov Knezevic <knezevic.jakov@gmail.com>
2026-09-03 18:45:30 +0200

    Added server component support

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { devtools } from '@tanstack/devtools-vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
+import rsc from '@vitejs/plugin-rsc';
 import { nitro } from 'nitro/vite';
 import { defineConfig } from 'vite';
 
@@ -11,7 +12,12 @@ const config = defineConfig({
 		devtools(),
 		nitro({ rollupConfig: { external: [/^@sentry\//] } }),
 		tailwindcss(),
-		tanstackStart(),
+		tanstackStart({
+			rsc: {
+				enabled: true,
+			},
+		}),
+		rsc(),
 		viteReact(),
 	],
 });

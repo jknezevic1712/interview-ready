6e56d4b3cd3cd37dc5415e44e531ea68a04c6f9e
Jakov Knezevic <knezevic.jakov@gmail.com>
2026-09-23 18:47:25 +0200

    [Next.js] primary-dark color removed

--- frontend-next/src/features/header/header.tsx
+++ frontend-next/src/features/header/header.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/shared/components/atoms/button';
 
 export function Header() {
 	return (
-		<div className="w-full flex justify-end align-center py-2 px-4 border-b-2 border-primary-dark mb-16">
+		<div className="w-full flex justify-end align-center py-2 px-4 border-b-2 border-primary mb-16">
 			<Button type="button">USER</Button>
 		</div>
 	);
